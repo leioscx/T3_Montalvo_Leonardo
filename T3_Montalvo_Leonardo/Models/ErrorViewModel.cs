b7d8449bf3d3119a6b14c9a56229eb0bf3dfc51d
Leonardo Montalvo <leomx329@gmail.com>
2025-06-19 02:51:24 +0200

    creando el proyecto

---
+++ T3_Montalvo_Leonardo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T3_Montalvo_Leonardo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
